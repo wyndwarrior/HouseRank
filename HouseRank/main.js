667a529856800cc357b29106e957004661426b35
Yu Xuan Liu <wynd07@gmail.com>
2016-04-02 12:58:34 -0700

    Added frontend

---
+++ HouseRank/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    postings = data["postings"];
+    probs = data["probs"]
+    design = data["design_matrix"]
+    for(var i = 0; i<probs.length; i++){
+        $("#tbody").append("<tr><td>asdf</td></tr>");
+    }
+})
