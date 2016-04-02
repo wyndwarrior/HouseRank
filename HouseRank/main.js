d5994ba7ec2cea9e03a7e79d15957af1c08c6838
Yu Xuan Liu <wynd07@gmail.com>
2016-04-02 13:08:51 -0700

    Finished front end

--- HouseRank/main.js
+++ HouseRank/main.js
@@ -2,7 +2,15 @@ $(document).ready(function(){
     postings = data["postings"];
     probs = data["probs"]
     design = data["design_matrix"]
+    bless = data["bless_down"]
     for(var i = 0; i<probs.length; i++){
-        $("#tbody").append("<tr><td>asdf</td></tr>");
+        p = postings[i]
+        d = design[i]
+        $("#tbody").append("<tr><td>"+probs[i].toFixed(5)+"</td>"+
+            "<td><a href='"+bless[i]+"' target='blank_'>"+p["title"]+"</a></td>"+
+            "<td>"+p["price"]+"</a></td>"+
+            "<td>"+d[25]+"</a></td>"+
+            "<td>"+d[31].toFixed(4)+" mi</a></td>"+
+            +"</tr>");
     }
 })
